41145628ecbb41fdba65e60e13e04ff8d7268bc7
Ben O'Sullivan <bigbeno37@gmail.com>
2017-08-11 09:46:41 +1000

    Show who won after game end

--- src/reversi.c
+++ src/reversi.c
@@ -5,7 +5,13 @@ int main()
     Player player1;
     Player player2;
 
-    playGame(&player1, &player2);
+    Player *winner = playGame(&player1, &player2);
+    
+    if (winner == NULL) {
+        puts("The game was a tie!");
+    } else {
+        printf("Congratulations %s, you won with %d points!\n", winner->name, winner->score); 
+    }
     
     puts("");
     puts("Goodbye. ");
